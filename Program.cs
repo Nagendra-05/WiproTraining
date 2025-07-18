a30530f2597e5ff1d3efdc3ac4f2ca17257270a5
Nagendra <nagengra@balla.com>
2025-07-18 18:31:17 +0530

    Day 1 Addition

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.Write("enter 1st number");
+            int a = Convert.ToInt16(Console.ReadLine());
+            Console.Write("enter 2nd number");
+            int b = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine($"sum of two numbers is:{a + b}");
+        }
+    }
+}
